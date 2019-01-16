592f1df4aa0540f7b3cf154ed9fbd8c6a2a03d90
DageHaohao <37402711+DageHaohao@users.noreply.github.com>
2019-01-16 10:45:37 +0800

    完成user注册接口功能，测试通过

--- src/main/java/push/factory/UserFactory.java
+++ src/main/java/push/factory/UserFactory.java
@@ -1,8 +1,10 @@
 package push.factory;
 
-import org.hibernate.Session;
 import push.bean.db.User;
 import push.utils.Hib;
+import push.utils.TextUtil;
+
+import java.util.UUID;
 
 /**
  * @author 黄君豪
@@ -11,27 +13,129 @@ import push.utils.Hib;
  * @Description: TODO
  * @date 2019/1/1521:09
  */
+
+
+
 public class UserFactory {
 
+    // 通过Token字段查询用户信息
+    // 只能自己使用，查询的信息是个人信息，非他人信息
+    public static User findByToken(String token) {
+        return Hib.query(session -> (User) session
+                .createQuery("from User where token=:token")
+                .setParameter("token", token)
+                .uniqueResult());
+    }
+
+    // 通过Phone找到User
+    public static User findByPhone(String phone) {
+        return Hib.query(session -> (User) session
+                .createQuery("from User where phone=:inPhone")
+                .setParameter("inPhone", phone)
+                .uniqueResult());
+    }
+
+    // 通过Name找到User
+    public static User findByName(String name) {
+        return Hib.query(session -> (User) session
+                .createQuery("from User where name=:name")
+                .setParameter("name", name)
+                .uniqueResult());
+    }
+
+    /**
+     * 更新用户信息到数据库
+     *
+     * @param user User
+     * @return User
+     */
+    public static User update(User user) {
+        return Hib.query(session -> {
+            session.saveOrUpdate(user);
+            return user;
+        });
+    }
+
+
+    /**
+     * 用户注册
+     * 注册的操作需要写入数据库，并返回数据库中的User信息
+     *
+     * @param account  账户
+     * @param password 密码
+     * @param name     用户名
+     * @return User
+     */
     public static User register(String account, String password, String name){
+
+        // 去除账户中的首位空格
+        account = account.trim();
+
+        password = encodePassword(password);
+
+        User user = createUser(account,password,name);
+
+        if(user!=null){
+            user = login(user);
+        }
+        return user;
+    }
+
+    /**
+     * 注册部分的新建用户逻辑
+     *
+     * @param account  手机号
+     * @param password 加密后的密码
+     * @param name     用户名
+     * @return 返回一个用户
+     */
+
+    private static User createUser(String account,String password,String name){
 
         User user = new User();
         user.setName(name);
         user.setPhone(account);
         user.setPassword(password);
 
-        Session session = Hib.session();
-        session.beginTransaction();
-        try {
+        // 数据库存储
+        return Hib.query(session -> {
             session.save(user);
-            session.getTransaction().commit();
             return user;
-        }catch (Exception e){
-            session.getTransaction().rollback();
-            e.printStackTrace();
-            return null;
-        }
+        });
 
+    }
+
+    /**
+     * 把一个User进行登录操作
+     * 本质上是对Token进行操作
+     *
+     * @param user User
+     * @return User
+     */
+    private static User login(User user) {
+        // 使用一个随机的UUID值充当Token
+        String newToken = UUID.randomUUID().toString();
+        // 进行一次Base64格式化
+        newToken = TextUtil.encodeBase64(newToken);
+        user.setToken(newToken);
+
+        return update(user);
+    }
+
+    /**
+     * 对密码进行加密操作
+     *
+     * @param password 原文
+     * @return 密文
+     */
+    private static String encodePassword(String password) {
+        // 密码去除首位空格
+        password = password.trim();
+        // 进行MD5非对称加密，加盐会更安全，盐也需要存储
+        //加盐方案：密码+当前随机的一个时间再进行md5加密
+        password = TextUtil.getMD5(password);
+        // 再进行一次对称的Base64加密，当然可以采取加盐的方案
+        return TextUtil.encodeBase64(password);
     }
 
 }
